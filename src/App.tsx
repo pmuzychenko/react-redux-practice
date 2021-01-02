9433d1c4fb1160cce1cac015a43d4560f6528669
pmuzychenko <pmuzychenko@gmail.com>
2021-01-02 15:21:18 +0200

    feat: add Rating, Accordion and Star components (Lesson 1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+    return (
+        <div>
+            This is App Component
+            <Rating/>
+            <Accordion/>
+        </div>
+    );
+}
+
+function Rating() {
+    return (
+        <div>
+            <Star/>
+        </div>
+    )
+}
+
+function Star() {
+    return <>
+        <div>Star</div>
+        <div>Star</div>
+        <div>Star</div>
+        <div>Star</div>
+        <div>Star</div>
+    </>
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
