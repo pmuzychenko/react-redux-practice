1440675c469aa77cf7333114841062e6cdbf7152
pmuzychenko <pmuzychenko@gmail.com>
2021-01-04 23:46:38 +0200

    feat: add stories for Controlled components (Lesson 13)

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -27,6 +27,36 @@ export const GetValueOfUncontrolledInput = () => {
         -- {value}
     </>
 }
+export const ControlledInput = () => {
+  const [value, setValue] = useState('')
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value)
+  }
+  return <> <input value={value} onChange={onChange}/> </>
+}
+export const ControlledCheckbox = () => {
+  const [value, setValue] = useState(true)
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.checked)
+  }
+  return <> <input type={'checkbox'} checked={value} onChange={onChange}/> </>
+}
+export const ControlledSelect = () => {
+  const [value, setValue] = useState<string | undefined>('1')
+  const onChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setValue(event.currentTarget.value)
+  }
+  return <select value={value} onChange={onChange}>
+  <option>None</option>
+  <option value={'1'}>Kiev</option>
+  <option value={'2'}>Minsk</option>
+  <option value={'3'}>Moscow</option>
+   </select>
+}
+
+
+
+
 export const ControlledInputWithFixedValue = () => <input value={'It-incubator.by'}/>
 
 
